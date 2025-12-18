aacd3d07ffc9abebef061f61dbc4f5201273a88a
arroyo-raul <arroyoguardiaraul@gmail.com>
2025-12-18 17:57:06 +0000

    Navbar creado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,20 @@ export const Navbar = () => {
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<img src="https://1000logos.net/wp-content/uploads/2017/06/Star-Wars-Logo-1.png" className="image-nav" />
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+					<div class="dropdown">
+						<a class="btn btn-primary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+							Favorites <span class="badge bg-secondary">4</span>
+						</a>
+
+						<ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+							<li><a class="dropdown-item" href="#">Action</a></li>
+							<li><a class="dropdown-item" href="#">Another action</a></li>
+							<li><a class="dropdown-item" href="#">Something else here</a></li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</nav>
